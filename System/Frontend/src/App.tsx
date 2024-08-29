a833ded71110f46fee2ad151ba5186cc3b738730
Marcel Hasselberg <marcel.hasselberg@mercedes-benz.com>
2024-08-29 11:04:59 +0200

    16-27-hot-fix

--- System/Frontend/src/App.tsx
+++ System/Frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import APIList from './components/apis/APIList';
 import ProtectedRoute from './components/route/ProtectedRoute';
 import Register from './components/login/Register';
