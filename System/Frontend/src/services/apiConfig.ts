d0dd49ab81e4acefa127906e7b566270e4227160
Michael Schick <michael.schick@gmx.net>
2024-08-30 11:22:07 +0200

    16-54 Changed wrong constant name

--- System/Frontend/src/services/apiConfig.ts
+++ System/Frontend/src/services/apiConfig.ts
@@ -1,5 +1,5 @@
 import constants from '../commonRessources/constants.json';
 
 export const POSTGRES_API_BASE_URL = constants.POSTGRES_DATA_CONNECTOR_URL + '/';
-export const INFLUX_API_BASE_URL = constants.INFLUX_API_URL + '/';
+export const INFLUX_API_BASE_URL = constants.INFLUX_DATA_CONNECTOR_URL + '/';
 export const SCHEDULER_API_BASE_URL = constants.SCHEDULER_API_URL + '/';
