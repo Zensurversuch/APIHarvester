ef872ac0f98c43cecd215bec9b728bec9a2c408f
Michael Schick <michael.schick@gmx.net>
2024-09-01 20:04:19 +0200

    58-60 Made Load Balancer for scheduler working

--- System/Scheduler/app.py
+++ System/Scheduler/app.py
@@ -187,4 +187,4 @@ def refreshOfelia():
         return f"Error restarting Ofelia container: {str(e)}"
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000, debug=True)
+      app.run(host='0.0.0.0', port=5002, debug=True)
